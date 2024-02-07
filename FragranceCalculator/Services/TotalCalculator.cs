626b9e2d3247be6241a821f11324d5163d831311
Maciej Kolacinski <hello@mattkaydev.com>
2024-02-07 22:27:53 +0000

    Created new Calculator class to handle calculations correctly

---
+++ FragranceCalculator/Services/TotalCalculator.cs
@@ -0,0 +1,31 @@
+﻿namespace FragranceCalculator.Services
+{
+    public class TotalCalculator
+    {
+        /// <summary>
+        /// Calculates total wax weight for all vessel.
+        /// </summary>
+        /// <param name="percentage">Required fragrance percentage</param>
+        /// <returns>Total wax weight</returns>
+        public double CalculateTotalWaxGrams(int totalGrams, int percentage)
+        {
+            var totalWax = totalGrams - this.CalculateFragrance(totalGrams, percentage);
+
+            return totalWax;
+        }
+
+        /// <summary>
+        /// Calculates fragrance in grams from the percentage given.
+        /// </summary>
+        /// <param name="totalGrams">Grams used as a total.</param>
+        /// <param name="requiredPercentage">The required percentage of fragrance.</param>
+        /// <returns></returns>
+        public double CalculateFragrance(int totalGrams,  int requiredPercentage)
+        {
+            var one = totalGrams / 100.0;
+            var totalPercentage = one * requiredPercentage;
+
+            return totalPercentage;
+        }
+    }
+}
